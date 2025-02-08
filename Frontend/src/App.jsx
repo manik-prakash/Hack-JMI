ae700251af5c4f672ce6f000732db33a905974a6
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2025-02-08 19:40:15 +0530

    Starting Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: 
+      <>
+        <Navbar/>
+        <Home/>
+      </>,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+  ]);
   return (
     <>
-    <h1>Hello</h1>
+      <RouterProvider router={routes} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
