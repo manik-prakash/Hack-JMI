4aa1c5194266d445741d2ad7e21550765565904e
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2025-02-08 18:05:56 +0530

    Second Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,35 @@
-// src/App.js
-import React from 'react';
-import Sidebar from './components/Sidebar';
-import Overview from './components/Overview';
-import RecentTransactions from './components/RecentTransactions';
-import AnomalyAlerts from './components/AnomalyAlerts';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="w-full p-4">
-        <Overview />
-        <div className="flex mt-4">
-          <div className="w-2/3 mr-4">
-            <RecentTransactions />
-          </div>
-          <div className="w-1/3">
-            <AnomalyAlerts />
-          </div>
-        </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
